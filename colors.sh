1a5848b5dfa1892210692181dc125ce3b3ebf38b
Ryan Cook <80371197+ryanc410@users.noreply.github.com>
2022-06-27 12:11:38 -0500

    Create colors.sh

---
+++ colors.sh
@@ -0,0 +1,27 @@
+########################################
+# Color Variables
+# Source this file to your .bashrc file
+########################################
+
+export start="\e[0;"
+export START="\e[1;"
+
+export red="31"
+export green="32"
+export orange="33"
+export purple="34"
+export cyan="36"
+export lightgray="37"
+
+export nobg="m"
+export bg=";"
+
+export bg0="40m"
+export bg1="41m"
+export bg2="42m"
+export bg3="43m"
+export bg4="44m"
+export bg5="46m"
+export bg6="47m"
+
+export end="\e[0m"
